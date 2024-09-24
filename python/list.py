40c843ce198c00563a05c18b3d25fa80ec42fabc
anandpb44 <anandpb44@gmail.com>
2024-09-24 16:02:07 +0530

    python

---
+++ python/list.py
@@ -0,0 +1,8 @@
+a=[1,2,3,4,5]
+a.remove(3)
+print("list",a)
+
+# for i in a:
+#     print(i)
+#print(a[2])
+#print(a[-1:-3])
---
+++ python/twonum.py
@@ -0,0 +1,6 @@
+num=[2,7,11,15]
+target=9
+print(num)
+for i in num(i):
+    if num[i]+num[i+1]==9:
+        print(num[i,i+1])
