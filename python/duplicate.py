4026ec888f4626775616c5bf4716bd4919eaa963
anandpb44 <anandpb44@gmail.com>
2024-09-27 16:59:02 +0530

    progress

--- python/duplicate.py
+++ python/duplicate.py
@@ -1,4 +1,6 @@
 l=[2,3,4,7,3,4,8]
+a=[]
 for i in l:
-    if 
-    jnj
+    if i not in a:
+        a.append(i)
+print(a)
---
+++ python/revers.py
@@ -0,0 +1,8 @@
+l=["apple","orange","kiwi"]
+for i in l:
+    print(i[::-1])
+# a="English"
+# print(a[0:5])
+# print(a[:])
+# print(a[::-1])
+# print(a[0:6:2])
